44669b912adde1b606b986d5b5da54f29fc9c3d0
Shirajhussain <shirajhussain2005@gmail.com>
2026-07-01 22:52:58 +0530

    Add GFG Minimum Platforms solution

--- Stack/1.Find_Index_Of_Closing_Bracket.cpp
+++ Stack/1.Find_Index_Of_Closing_Bracket.cpp
@@ -1,13 +1,38 @@
-class Solution {
-  public:
-    int closing(string &s, int pos) {
-        int cnt = 0;
-        for (int i = pos; i < s.size(); i++) {
-            if (s[i] == '[') cnt++;
-            else if (s[i] == ']') cnt--;
+#include<iostream>
+using namespace std;
 
-            if (cnt == 0) return i;
+    
+
+// class Solution {
+//   public:
+//     int closing(string &s, int pos) {
+//         int cnt = 0;
+//         for (int i = pos; i < s.size(); i++) {
+//             if (s[i] == '[') cnt++;
+//             else if (s[i] == ']') cnt--;
+
+//             if (cnt == 0) return i;
+//         }
+//     return -1;
+//     }
+// };
+bool LinearSearch(int arr[] , int target , int n){
+    for(int i=0; i<n; i++){
+        if(arr[i] == target){
+            return true;
         }
-    return -1;
     }
-};
+    return false;
+}
+
+int main(){
+    int arr[] = {2,4,6,8,10};
+    int target = 6;
+    int n=5;
+    LinearSearch(arr,target,n);
+
+    return 0;
+}
+
+
+
